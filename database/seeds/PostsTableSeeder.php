a8de3e06c97d9cd9a626398f804596c4621f8297
Annalisa <annalisadesantis770@gmail.com>
2021-01-25 17:24:48 +0100

    aggiunta valori nei faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -16,10 +16,10 @@ class PostsTableSeeder extends Seeder
         for ($i=0; $i < 100; $i++) {
 
             $new_post = new Post();
-            $new_post->title = $faker->sentence();
-            $new_post->subtitle = $faker->sentences();
+            $new_post->title = $faker->sentence(3);
+            $new_post->subtitle = $faker->sentence(2);
             $new_post->author = $faker->name();
-            $new_post->text = $faker->text();
+            $new_post->text = $faker->text(100);
             $new_post->date = $faker->date();
             $new_post->time = $faker->time();
             $new_post->save();
