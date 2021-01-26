a3d11e3d24db5fc1b9934d920de1a6dd93aee42c
Annalisa <annalisadesantis770@gmail.com>
2021-01-26 20:38:08 +0100

    aggiunta rotta post + visualizzazione in pagina

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Auth::routes(['register' => false]);
 Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
 
     Route::get('/', 'HomeController@index')->name('index');
+    Route::resource('/posts', 'PostController');
 });
