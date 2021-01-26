d5c061dfcc1b8699f47d41e930fad17bfd9aa644
Annalisa <annalisadesantis770@gmail.com>
2021-01-26 16:58:19 +0100

    divisione delle rotte pubbliche e private

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Rotta pubblica
+Route::get('/', 'HomeController@index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', 'Admin\HomeController@index')->name('dashboard')->middleware('auth');
+
+// Rotta privata
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
+
+    Route::get('/', 'HomeController@index')->name('index');
+});
