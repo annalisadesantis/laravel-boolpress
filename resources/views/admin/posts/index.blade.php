9ca600589fd526e9fa80964fd80298e104437c57
Annalisa <annalisadesantis770@gmail.com>
2021-01-27 15:30:55 +0100

    creazione view posts.create

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,7 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>Tutti i posts</h1>
+            <div class="d-flex justify-content-between align-items-center">
+                <h1>Tutti i posts</h1>
+                <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">
+                    Crea nuovo post
+                </a>
+            </div>
             <table class="table">
                 <thead>
                     <tr>
@@ -21,7 +26,7 @@
                             <td>{{ $post->title }}</td>
                             <td>{{ $post->slug }}</td>
                             <td>
-                                <a class="btn btn-info btn-sm" href="#">
+                                <a class="btn btn-info btn-sm" href="{{ route('admin.posts.show', ['post' => $post->id ]) }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
                                 </a>
                                 <a class="btn btn-warning btn-sm" href="#">
