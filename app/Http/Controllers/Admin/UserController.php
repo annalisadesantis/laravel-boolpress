2a0998dfaae8378d7b791197b09d93499f7dd485
Annalisa <annalisadesantis770@gmail.com>
2021-01-28 15:14:40 +0100

    UserController + view users

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $data = [
+           'users' => User::all()
+       ];
+       return view('admin.users.index', $data);
+    }
+}
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="d-flex justify-content-between align-items-center">
+                <h1>Tutti gli users</h1>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
