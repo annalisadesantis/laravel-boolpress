9d0659b50ba9aa237d1457b47150441ff3f6b877
Annalisa <annalisadesantis770@gmail.com>
2021-01-31 13:08:10 +0100

    aggiunta nome del admin nella navbar + creazione nuovo admin

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -21,7 +21,9 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('index')}}">Boolpress</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('index')}}">
+            Boolpress: {{ Auth::user()->name }}
+        </a>
         <ul class="navbar-nav px-3 ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('index') }}">
