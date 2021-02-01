cda3534a0b99d6411330eafbac43fe8996ab0536
Annalisa <annalisadesantis770@gmail.com>
2021-02-01 17:13:50 +0100

    compilazione e esecuzione api_token migratrion

--- database/migrations/2021_02_01_160929_add_api_token_users_table.php
+++ database/migrations/2021_02_01_160929_add_api_token_users_table.php
@@ -14,7 +14,10 @@ class AddApiTokenUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('api_token', 80)->after('password')
+                        ->unique()
+                        ->nullable()
+                        ->default(null);
         });
     }
 
@@ -26,7 +29,7 @@ class AddApiTokenUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('api_token');
         });
     }
 }
